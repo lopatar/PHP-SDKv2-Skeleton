93b3c7f9ac31cea3fe9d3d04dc23aac9c8deb0b7
Jiří Lopatář <github@lopatar.me>
2023-01-12 18:30:50 +0100

    Add example of HttpBasicAuth.php middleware in index.php

--- public/index.php
+++ public/index.php
@@ -5,9 +5,18 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Config;
 use Sdk\App;
+use Sdk\Middleware\HttpBasicAuth;
 
 $config = new Config();
 $app = new App($config);
+
+$users = [
+	['username' => 'test', 'password' => '$2y$12$3W.teM8Ph5cp4CwZy9r0D.MtI.RYLW0kSsYpfvTrBio8tBevQCG2m']
+];
+
+$authMiddleware = new HttpBasicAuth($users, 'Please authenticate');
+
+$app->addMiddleware($authMiddleware);
 
 $app->get('/{username}', 'Home::render')
 	?->whereParam('username')
